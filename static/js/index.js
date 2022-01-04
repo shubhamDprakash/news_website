7898e68275c63fc1ef314bb789db806ee523ea03
Shubham <shubhamprakash702@gmail.com>
2022-01-04 21:48:52 +0530

    made the search option in nav bar functional

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-console.log('This is Shubham');
+console.log('Hi this is Shubham\nContact at shubhamprakash702@gmail.com for any issue');
 // news_api_key = 2ddd527e51644ef8a3b329adf0bc718d (news.org)
 
 // grab the news container
@@ -28,16 +28,16 @@ function putNews(data){
     articles.forEach((element,index) => {
         // console.log(index, element);
         let news = `
-            <div class="card-fluid w-75 my-3" style="border: 1px solid rgb(162, 162, 241);margin: auto;">
+            <div class="card-fluid w-75 my-3 newsBox" style="border: 1px solid rgb(162, 162, 241);margin: auto;">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${element["urlToImage"]}" class="img-fluid rounded-start" alt="static/images/no_img.jpg">
+                        <img src="${element["urlToImage"]}" class="img-fluid rounded-start" alt="Image Not Found">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">${element["title"]}</h5>
-                            <p class="card-text">${element["content"]} <a href="${element["url"]}" target="_blank">Go to article</a></p>
-                            <p class="card-text"><small class="text-muted">By - ${element["source"]["name"]}</small></p>
+                            <h5 class="card-title search">${element["title"]}</h5>
+                            <p class="card-text search">${element["content"]} <a href="${element["url"]}" target="_blank">Go to article</a></p>
+                            <p class="card-text search"><small class="text-muted">By - ${element["source"]["name"]}</small></p>
                             <p class="card-text"><small class="text-muted">publishedAt - ${element["publishedAt"]}</small></p>
                         </div>
                     </div>
@@ -55,7 +55,7 @@ function putNews(data){
                         </h2>
                         <div id="flush-collapse${index}" class="accordion-collapse collapse"
                             aria-labelledby="flush-heading${index}" data-bs-parent="#accordionFlushExample${index}">
-                            <div class="accordion-body">${element["description"]}</div>
+                            <div class="accordion-body search">${element["description"]}</div>
                         </div>
                     </div>
                 </div>
@@ -65,5 +65,35 @@ function putNews(data){
     myNews.innerHTML = newsHtml;
 }
 
+// made the search option functional
+let search = document.getElementById('searchTxt');
+let counter;
 
+search.addEventListener("input", function(){
+    let inputVal = search.value.toLowerCase();
+    // console.log(inputVal);
+    let newsBox = document.getElementsByClassName('newsBox');
+    Array.from(newsBox).forEach((element)=>{
+        let newsSearch = element.getElementsByClassName('search');
+        Array.from(newsSearch).forEach((para)=>{
+            searchText = para.innerText.toLowerCase();
+            if (searchText.includes(inputVal)){
+                counter++;
+            }
+            // console.log(searchText);
+        })
+        if (counter > 0){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+        counter = 0;
+    })
+})
+
+let searchSubmit = document.getElementById('searchSubmit')
+searchSubmit.addEventListener("click", (e)=>{
+    e.preventDefault();
+})
 
